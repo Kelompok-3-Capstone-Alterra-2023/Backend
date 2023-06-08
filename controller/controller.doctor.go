675ffe64fe2ca8afc68d09eb14ce2ea054583804
habil <habiedgar111@gmail.com>
2023-06-08 22:01:19 +0700

    [fix] route doctor login and env

--- controller/controller.doctor.go
+++ controller/controller.doctor.go
@@ -1,21 +1,18 @@
 package controller
 
 import (
-
+	"errors"
 	"net/http"
 	"strconv"
-	"errors"
 
-	"capstone/model"
 	"capstone/config"
+	"capstone/model"
 
-	"github.com/labstack/echo/v4"
 	"github.com/jinzhu/gorm"
-  
-  
+	"github.com/labstack/echo/v4"
+
 	"capstone/lib/email"
 	"capstone/middleware"
-
 )
 
 // for admin
@@ -26,17 +23,17 @@ func (a *DoctorAdminController) GetDoctors(c echo.Context) error {
 	var doctors []model.Doctor
 
 	config.DB.Find(&doctors)
-  
+
 	if err := config.DB.Find(&doctors).Error; err != nil {
-	  return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 	return c.JSON(http.StatusOK, map[string]interface{}{
-	  "message": "success get all doctors",
-	  "doctors":   doctors,
+		"message": "success get all doctors",
+		"doctors": doctors,
 	})
-  }
+}
 
-//get doctor by id
+// get doctor by id
 func (a *DoctorAdminController) GetDoctor(c echo.Context) error {
 	// Bind request data to doctor struct
 	doctor := model.Doctor{}
@@ -53,7 +50,7 @@ func (a *DoctorAdminController) GetDoctor(c echo.Context) error {
 	return c.JSON(http.StatusOK, doctor)
 }
 
-//delete doctor
+// delete doctor
 func (a *DoctorAdminController) DeleteDoctor(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
@@ -77,7 +74,7 @@ func (a *DoctorAdminController) DeleteDoctor(c echo.Context) error {
 	return c.JSON(http.StatusOK, echo.Map{"message": "Doctor deleted successfully"})
 }
 
-//update doctor
+// update doctor
 func (a *DoctorAdminController) UpdateDoctor(c echo.Context) error {
 	data := echo.Map{
 		"message": "success update doctor",
@@ -119,13 +116,13 @@ func (d *DoctorDoctorController) GetDoctors(c echo.Context) error {
 	var doctors []model.Doctor
 
 	config.DB.Find(&doctors)
-  
+
 	if err := config.DB.Find(&doctors).Error; err != nil {
-	  return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 	return c.JSON(http.StatusOK, map[string]interface{}{
-	  "message": "success get all doctors",
-	  "doctors":   doctors,
+		"message": "success get all doctors",
+		"doctors": doctors,
 	})
 }
 
@@ -137,39 +134,40 @@ func (u *DoctorUserController) GetDoctors(c echo.Context) error {
 	var doctors []model.Doctor
 
 	config.DB.Find(&doctors)
-  
+
 	if err := config.DB.Find(&doctors).Error; err != nil {
-	  return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 	return c.JSON(http.StatusOK, map[string]interface{}{
-	  "message": "success get all doctors",
-	  "doctors":   doctors,
-
+		"message": "success get all doctors",
+		"doctors": doctors,
+	})
+}
 
 func CreateDoctor(c echo.Context) error {
 	var doctor model.Doctor
 	var otp model.OTP
 	c.Bind(&doctor)
 	tempOTP := c.FormValue("otp")
-	if tempOTP == ""{
+	if tempOTP == "" {
 		otp.OTP = email.GenerateOTP()
 		otp.DoctorEmail = doctor.Email
-		if err:= email.SendEmail("test",doctor.Email,otp.OTP); err!= nil{
+		if err := email.SendEmail("test", doctor.Email, otp.OTP); err != nil {
 			return c.JSON(500, map[string]interface{}{
 				"message": "Failed to send OTP",
 			})
 		}
 		err := config.DB.Where("doctor_email=?", doctor.Email).Save(&otp).Error
-		if err != nil{
+		if err != nil {
 			return c.JSON(500, map[string]interface{}{
 				"message": "Failed to save doctor email",
-			})	
+			})
 		}
 		return c.JSON(200, map[string]interface{}{
 			"message": "Please check your email",
 		})
-	}else{
-		if err:=config.DB.Where("doctor_email = ? AND otp = ?", doctor.Email, tempOTP).First(&otp).Error; err != nil{
+	} else {
+		if err := config.DB.Where("doctor_email = ? AND otp = ?", doctor.Email, tempOTP).First(&otp).Error; err != nil {
 			return c.JSON(500, map[string]interface{}{
 				"message": "Wrong OTP",
 			})
@@ -182,28 +180,28 @@ func CreateDoctor(c echo.Context) error {
 	}
 	return c.JSON(200, map[string]interface{}{
 		"message": "success create doctor",
-		"data": doctor,
+		"data":    doctor,
 	})
 }
 
-func LoginDoctor(c echo.Context) error{
+func LoginDoctor(c echo.Context) error {
 	var doctor model.Doctor
 	c.Bind(&doctor)
-	if err := config.DB.Where("email = ? AND password = ?", doctor.Email, doctor.Password).First(&doctor).Error; err != nil{
+	if err := config.DB.Where("email = ? AND password = ?", doctor.Email, doctor.Password).First(&doctor).Error; err != nil {
 		return c.JSON(500, map[string]interface{}{
 			"message": "failed to login",
-			"error": err.Error(),
+			"error":   err.Error(),
 		})
 	}
 	token, err := middleware.CreateDoctorJWT(doctor.ID)
-	if err != nil{
+	if err != nil {
 		return c.JSON(500, map[string]interface{}{
 			"message": "failed to login",
-			"error": err.Error(),
+			"error":   err.Error(),
 		})
 	}
 	return c.JSON(200, map[string]interface{}{
 		"message": "success login",
-		"token": token,
+		"token":   token,
 	})
 }
--- route/route.go
+++ route/route.go
@@ -1,29 +1,26 @@
 package route
 
 import (
-
 	"capstone/constant"
 	"capstone/controller"
 	m "capstone/middleware"
 
 	jwtMid "github.com/labstack/echo-jwt"
 
-
 	"github.com/labstack/echo/v4"
 )
-
 
 func New() *echo.Echo {
 	e := echo.New()
 	m.LogMiddleware(e)
 
 	articleUserController := controller.ArticleUserController{}
-  	doctorUserController := controller.DoctorUserController{}
+	doctorUserController := controller.DoctorUserController{}
 	eUser := e.Group("user")
 	eUser.GET("/articles", articleUserController.GetArticles)
 	eUser.GET("/articles/:id", articleUserController.GetDetailArticle)
 	eUser.GET("/articles/search", articleUserController.SearchArticles)
-  	eUser.GET("/doctors", doctorUserController.GetDoctors)
+	eUser.GET("/doctors", doctorUserController.GetDoctors)
 	eUser.POST("/register", controller.RegisterUser)
 	eUser.POST("/login", controller.LoginUser)
 	eUser.GET("/", controller.GetUser, m.MiddlewareJWT)
@@ -31,30 +28,30 @@ func New() *echo.Echo {
 	eUser.PUT("/", controller.UpdateUser, m.MiddlewareJWT)
 
 	articleDoctorController := controller.ArticleDoctorController{}
-  doctorDoctorController := controller.DoctorDoctorController{}
+	doctorDoctorController := controller.DoctorDoctorController{}
 	eDoc := e.Group("doctor")
 	eDoc.Use(jwtMid.JWT([]byte(constant.JWT_SECRET_KEY)))
-  eDoc.POST("/register", controller.CreateDoctor)
-	eDoc.POST("/login", controller.LoginDoctor)
+	e.POST("/doc/register", controller.CreateDoctor)
+	e.POST("/doc/login", controller.LoginDoctor)
 	eDoc.POST("/articles", articleDoctorController.AddArticle)
 	eDoc.PUT("/articles/:id", articleDoctorController.UpdateArticle)
 	eDoc.DELETE("/articles/:id", articleDoctorController.DeleteArticle)
 	eDoc.GET("/articles", articleDoctorController.GetArticles)
 	eDoc.GET("/articles/search", articleDoctorController.SearchArticles)
-  eDoc.GET("/doctors", doctorDoctorController.GetDoctors)
+	eDoc.GET("/doctors", doctorDoctorController.GetDoctors)
 
 	articleAdminController := controller.ArticleAdminController{}
-  doctorAdminController := controller.DoctorAdminController{}
+	doctorAdminController := controller.DoctorAdminController{}
 	eAdm := e.Group("admin")
 	eAdm.GET("/articles", articleAdminController.GetArticles)
 	eAdm.GET("/articles/:id", articleAdminController.GetDetailArticle)
 	eAdm.PUT("/articles/:id", articleAdminController.AcceptArticle)
 	eAdm.DELETE("/articles/:id", articleAdminController.DeleteArticle)
 	eAdm.GET("/articles/search", articleAdminController.SearchArticles)
-  eAdm.GET("/doctors", doctorAdminController.GetDoctors)
+	eAdm.GET("/doctors", doctorAdminController.GetDoctors)
 	eAdm.GET("/doctor/:id", doctorAdminController.GetDoctor)
 	eAdm.PUT("/doctor/:id", doctorAdminController.UpdateDoctor)
 	eAdm.DELETE("/doctor/:id", doctorAdminController.DeleteDoctor)
-  
+
 	return e
 }
