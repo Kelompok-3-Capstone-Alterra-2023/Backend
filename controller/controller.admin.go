14b646bd8b490e381d7868306c92c79d81ff3fd8
quniverse26 <quniverse26@gmail.com>
2023-06-16 07:56:07 +0700

    [fix] admin login & doctor approve

--- controller/controller.admin.go
+++ controller/controller.admin.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+    "gorm.io/gorm"
 
 	"capstone/config"
 	"capstone/model"
@@ -11,27 +12,33 @@ import (
 )
 
 func LoginAdmin(c echo.Context) error {
-	var admin model.Admin
-	admin.Username = "iamadmin"
-	admin.Password = "password"
+    var admin model.Admin
+    admin.Username = "admin"
+    admin.Password = "password"
 
-	if err := config.DB.Where("username = ? AND password = ?", admin.Username, admin.Password).First(&admin).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"message": "failed to login",
-			"error":   err.Error(),
-		})
-	}
+    if err := config.DB.Where("username = ? AND password = ?", admin.Username, admin.Password).First(&admin).Error; err != nil {
+        if err == gorm.ErrRecordNotFound {
+            return c.JSON(http.StatusUnauthorized, map[string]interface{}{
+                "message": "invalid credentials",
+            })
+        }
+        return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+            "message": "failed to login",
+            "error":   err.Error(),
+        })
+    }
 
-	token, err := middleware.CreateAdminJWT(admin.ID)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"message": "failed to login",
-			"error":   err.Error(),
-		})
-	}
+    token, err := middleware.CreateAdminJWT(admin.ID)
+    if err != nil {
+        return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+            "message": "failed to login",
+            "error":   err.Error(),
+        })
+    }
 
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "success login",
-		"token":   token,
-	})
+    return c.JSON(http.StatusOK, map[string]interface{}{
+        "message": "success login",
+        "token":   token,
+    })
 }
+
