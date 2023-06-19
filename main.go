48c60e4137c4a6ddd94964631dc031d7a2ea68cd
Rifkhi Akbar <rifkhihebat@upi.edu>
2023-06-19 19:13:14 +0700

    [fix] actions

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 	e := route.New()
 
-	if err := e.StartTLS(":8080", "/home/ubuntu/host.cert", "/home/ubuntu/host.key"); err != http.ErrServerClosed {
+	if err := e.StartTLS(":8080", "/app/fullchain.pem", "/app/privkey.pem"); err != http.ErrServerClosed {
 		log.Fatal(err)
 	  }
 }
