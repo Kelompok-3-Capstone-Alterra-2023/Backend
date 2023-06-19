a0a2aea3108f62f97efd74493a7bf35f1c6ce70d
Rifkhi Akbar Putra Rustandi <91230120+rifkeh@users.noreply.github.com>
2023-06-19 12:04:02 +0700

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 	e := route.New()
 
-	if err := e.StartTLS(":8080", "/home/ubuntu/host.cert", "/home/ubuntu/host.key"); err != http.ErrServerClosed {
+	if err := e.StartTLS(":8080", "/app/host.cert", "/app/host.key"); err != http.ErrServerClosed {
 		log.Fatal(err)
 	  }
 }
